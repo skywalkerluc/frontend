036abd31b531d6566235b672cbd6e00ab314cf6e
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-20 00:01:48 -0300

    feat(happy-button): use vue-ripple on HappyButton

--- components/HappyButton.vue
+++ components/HappyButton.vue
@@ -1,8 +1,17 @@
 <template>
-  <button :class="getClass">{{ label }}</button>
+  <button
+    v-ripple
+    :class="getClass">
+    {{ label }}
+  </button>
 </template>
 <script>
+import Ripple from 'vue-ripple-directive';
+
 export default {
+  directives: {
+    Ripple
+  },
   props: {
     label: {
       required: true,
@@ -25,6 +34,10 @@ export default {
           return 'info';
           break;
         }
+        case 'primary': {
+          return 'primary';
+          break;
+        }
         default: {
           return '';
         }
@@ -35,31 +48,48 @@ export default {
 </script>
 <style lang="scss" scoped>
 button {
-  background-color: orangered;
+  background-color: #f1f1f1;
   box-shadow: 0px 0px 45px -8px rgba(0,0,0,0.75);
-  color: white;
+  color: black;
   border: none;
-  border-radius: 20px;
-  font-size: 0.9em;
+  border-radius: 5px;
+  font-size: 0.8em;
   padding: 10px 20px;
   transition: all ease-in-out 100ms;
+  font-weight: 600;
+  text-transform: uppercase;
 
   &:hover {
-    background-color: darken(orangered, 10)
+    background-color: darken(#f1f1f1, 5)
+  }
+  &:disabled {
+    color: rgba(255,255,255,.75);
+    background-color: grey;
+    box-shadow: 0 15px 35px rgba(50,50,93,.1), 0 5px 15px rgba(0,0,0,.07);
   }
 }
 
 .danger {
   background-color: crimson;
+  color: white;
   &:hover {
-    background-color: darken(crimson, 10)
+    background-color: darken(crimson, 5)
   }
 }
 
 .info {
   background-color: blueviolet;
+  color: white;
   &:hover {
-    background-color: darken(blueviolet, 10)
+    background-color: darken(blueviolet, 5)
+  }
+}
+
+.primary {
+  background-color: orangered;
+  color: white;
+  &:hover {
+    background-color: darken(orangered, 5)
   }
 }
 </style>
