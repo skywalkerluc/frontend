44c45eade8b8ed9588c0ebe5e0beedef3fcbab8e
WandersonAlves <wandersonalvesferreira2012@gmail.com>
2018-07-23 14:45:45 -0300

    feat(happy-topbar): WIP component

---
+++ components/HappyTopbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="topbar">
+    <slot/>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.topbar {
+  width: 100%;
+  min-height: 300px;
+  display: flex;
+  background-color: orangered;
+  box-shadow: 0px 9px 39px 0px rgba(0,0,0,0.35);
+}
+</style>
+
+
--- pages/showcase/index.vue
+++ pages/showcase/index.vue
@@ -1,56 +1,59 @@
 <template>
   <section>
-    <p class="label">Botões</p>
-    <div class="flex">
-      <happy-button
-        type="danger"
-        label="Danger Button"
-      />
-      <happy-button
-        type="info"
-        label="Info Button"
-      />
-      <happy-button
-        type="primary"
-        label="Primary Button"
-      />
-      <happy-button
-        label="Normal Button"
-      />
-      <happy-button
-        disabled
-        label="Disabled Button"
-      />
-    </div>
-    <p class="label">Cards</p>
-    <div>
-      <happy-card
-        title="Titulo do card"
-        resume="ewhqoiuewhoiuwehouiqwehqowuiehqweouihweouihqew"
-        image="https://is3-ssl.mzstatic.com/image/thumb/Purple19/v4/44/d5/52/44d552f0-9ade-4714-652e-c0a9a6ef2ad7/pr_source.png/246x0w.jpg">
+    <happy-topbar/>
+    <div class="content">
+      <p class="label">Botões</p>
+      <div class="flex">
         <happy-button
-          slot="footer"
           type="danger"
-          label="Bem vindo ao Birrépi! =)"
+          label="Danger Button"
         />
-      </happy-card>
-    </div>
-    <div style="display: flex; flex-wrap: wrap;">
-      <div style="max-width: 50%; width: 50%;">
-        <happy-card
-          style="margin-right: 10px;"
-          title="Titulo do card"
-          resume="50% card size"/>
+        <happy-button
+          type="info"
+          label="Info Button"
+        />
+        <happy-button
+          type="primary"
+          label="Primary Button"
+        />
+        <happy-button
+          label="Normal Button"
+        />
+        <happy-button
+          disabled
+          label="Disabled Button"
+        />
       </div>
-      <div style="max-width: 50%; width: 50%;">
+      <p class="label">Cards</p>
+      <div>
         <happy-card
           title="Titulo do card"
-          resume="50% card size"/>
+          resume="ewhqoiuewhoiuwehouiqwehqowuiehqweouihweouihqew"
+          image="https://is3-ssl.mzstatic.com/image/thumb/Purple19/v4/44/d5/52/44d552f0-9ade-4714-652e-c0a9a6ef2ad7/pr_source.png/246x0w.jpg">
+          <happy-button
+            slot="footer"
+            type="danger"
+            label="Bem vindo ao Birrépi! =)"
+          />
+        </happy-card>
       </div>
-      <div style="max-width: 50%; width: 50%;">
-        <happy-card
-          title="Titulo do card"
-          resume="50% card size"/>
+      <div style="display: flex; flex-wrap: wrap;">
+        <div style="max-width: 50%; width: 50%;">
+          <happy-card
+            style="margin-right: 10px;"
+            title="Titulo do card"
+            resume="50% card size"/>
+        </div>
+        <div style="max-width: 50%; width: 50%;">
+          <happy-card
+            title="Titulo do card"
+            resume="50% card size"/>
+        </div>
+        <div style="max-width: 50%; width: 50%;">
+          <happy-card
+            title="Titulo do card"
+            resume="50% card size"/>
+        </div>
       </div>
     </div>
   </section>
@@ -59,19 +62,24 @@
 <script>
 import HappyButton from '../../components/HappyButton';
 import HappyCard from '../../components/HappyCard';
+import HappyTopbar from '../../components/HappyTopbar';
 
 export default {
   components: {
     HappyButton,
-    HappyCard
+    HappyCard,
+    HappyTopbar,
   }
 }
 </script>
 <style lang="scss" scoped>
 section {
-  margin: 20px;
   div {
     margin-bottom: 20px;
+  }
+
+  .content {
+    margin: 20px;
   }
 }
 .label {
