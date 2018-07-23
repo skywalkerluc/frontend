76e9fbfc8670e28e5fc0685a40e5c7903f1db826
WandersonAlves <wandersonalvesferreira2012@gmail.com>
2018-07-23 14:45:29 -0300

    feat(happy-card): add border

--- components/HappyCard.vue
+++ components/HappyCard.vue
@@ -38,6 +38,7 @@ export default {
 .happy-card {
   padding: 20px;
   box-shadow: 0 15px 35px rgba(50,50,93,.1), 0 5px 15px rgba(0,0,0,.07);
+  border: 1px solid rgba(0,0,0,.1);
   background-color: #fff;
   display: flex;
   flex-direction: column;
